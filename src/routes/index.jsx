dad53124c40f0abaf21db4c4526be71feafde8ff
ptsuarez91@hotmail.com <ptsuarez91@hotmail.com>
2024-01-07 22:26:54 -0300

    Primera configuracion inicial

---
+++ src/routes/index.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
+
+const AppRoutes = () => {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/"  />
+       
+      </Routes>
+    </Router>
+  );
+};
+
+export default AppRoutes;
