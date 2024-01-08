7bbbff33de0aff752fba6b9abc582d134c6d4cfe
jeermuce <ernesto_mc.dev@proton.me>
2024-01-07 23:09:12 -0600

    Add basic header component

---
+++ src/components/HeaderXL/HeaderXL.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import "../../styles/HeaderXL.css";
+
+function HeaderXL({ headerXL }) {
+  return <h1 className="header-xl">{headerXL}</h1>;
+}
+
+export default HeaderXL;
