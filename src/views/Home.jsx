6daee279a31d2f5f95a8ea827a6b107c48264b62
jeermuce <ernesto_mc.dev@proton.me>
2024-01-08 16:17:50 -0600

    Bring in Navbar component

--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -4,9 +4,11 @@ import PrimaryButton from "../components/PrimaryButton/PrimaryButton.jsx";
 import SecondaryButton from "../components/SecondaryButton/SecondaryButton.jsx";
 import HorizontalFlex from "../components/HorizontalFlex/HorizontalFlex.jsx";
 import "../styles/home.css";
+import Navbbar from "../components/Navbar/Navbbar.jsx";
 function Home() {
   return (
     <div className="home-page">
+      <Navbbar />
       <HeaderXL headerXL="Home" />
       <HorizontalFlex childrenMarginRight={"2 rem"}>
         <PrimaryButton label="Primary Button" />
