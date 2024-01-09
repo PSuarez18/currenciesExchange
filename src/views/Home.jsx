d97b8fe1ed9a96975b9a6c27955632178a74b3fb
jeermuce <ernesto_mc.dev@proton.me>
2024-01-08 21:01:13 -0600

    Update component imports and fix typos

--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -1,15 +1,15 @@
 import React from "react";
-import HeaderXL from "../components/HeaderXL/HeaderXL.jsx";
+import Header from "../components/Header/Header.jsx";
 import PrimaryButton from "../components/PrimaryButton/PrimaryButton.jsx";
 import SecondaryButton from "../components/SecondaryButton/SecondaryButton.jsx";
 import HorizontalFlex from "../components/HorizontalFlex/HorizontalFlex.jsx";
 import "../styles/home.css";
-import Navbbar from "../components/Navbar/Navbbar.jsx";
+import Navbar from "../components/Navbar/Navbar.jsx";
 function Home() {
   return (
     <div className="home-page">
-      <Navbbar />
-      <HeaderXL headerXL="Home" />
+      <Navbar />
+      <Header Header="Home" />
       <HorizontalFlex childrenMarginRight={"2 rem"}>
         <PrimaryButton label="Primary Button" />
         <SecondaryButton label="Secondary Button" />
