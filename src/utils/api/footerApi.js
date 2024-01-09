a27a8f7a0b03149dd5bdfe74f75e8af433be7608
ptsuarez91@hotmail.com <ptsuarez91@hotmail.com>
2024-01-09 02:44:06 -0300

    footer responsive testeado en pantallas de 22, 17 pulgadas, celular, falta el responsive de resolucion mayor.. Con enlaces de contacto funcionales, links aun por completar urls en la carpeta api para dejarlos funcionales

---
+++ src/utils/api/footerApi.js
@@ -0,0 +1,16 @@
+const socialIcons = {
+    whatsapp: {
+        api: "https://wa.me/+5493873641509",
+        icon: "src/assets/images/whatsapp.png",
+    },
+    facebook: {
+        api: "https://www.facebook.com/search/top?q=exchange%20divisas",
+        icon: "src/assets/images/facebook.png",
+    },
+    email: {
+        api: "mailto:ptsuarez91@hotmail.com",
+        icon: "src/assets/images/email.png",
+    },
+};
+
+export default socialIcons;
--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -4,14 +4,26 @@ import PrimaryButton from "../components/PrimaryButton/PrimaryButton.jsx";
 import SecondaryButton from "../components/SecondaryButton/SecondaryButton.jsx";
 import HorizontalFlex from "../components/HorizontalFlex/HorizontalFlex.jsx";
 import "../styles/home.css";
+import Footer from "../components/Footer/Footer.jsx";
 function Home() {
   return (
     <div className="home-page">
-      <HeaderXL headerXL="Home" />
-      <HorizontalFlex childrenMarginRight={"2 rem"}>
-        <PrimaryButton label="Primary Button" />
-        <SecondaryButton label="Secondary Button" />
-      </HorizontalFlex>
+      <header>
+        <HeaderXL headerXL="Home" />
+        <HorizontalFlex childrenMarginRight={"2 rem"}>
+          <PrimaryButton label="Primary Button" />
+          <SecondaryButton label="Secondary Button" />
+        </HorizontalFlex>
+
+      </header>
+      <main>
+
+      </main>
+      <footer>
+        <Footer />
+
+      </footer>
+
     </div>
   );
 }
