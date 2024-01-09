995fc8cedbd5f375c9d667fad012884cbfcefa65
jeermuce <ernesto_mc.dev@proton.me>
2024-01-09 11:42:45 -0600

    Delete HeaderXl component

--- src/components/Header/HeaderXL.jsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import "../../styles/Header.css";
-
-function Header({ Header }) {
-  return <h1 className="header-xl">{Header}</h1>;
-}
-
-export default Header;
--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -1,31 +1,26 @@
-import React from "react";import Navbar from "../components/Navbar/Navbar.jsx";import Footer from "../components/Footer/Footer.jsx";
-import HeaderXL from "../components/Header/HeaderXL.jsx";
+import React from "react";
+import Navbar from "../components/Navbar/Navbar.jsx";
+import Footer from "../components/Footer/Footer.jsx";
 import PrimaryButton from "../components/PrimaryButton/PrimaryButton.jsx";
 import SecondaryButton from "../components/SecondaryButton/SecondaryButton.jsx";
 import HorizontalFlex from "../components/HorizontalFlex/HorizontalFlex.jsx";
 import "../styles/home.css";
 
-
 function Home() {
   return (
     <div className="home-page">
       <header>
-      <Navbar />
-
+        <Navbar />
       </header>
       <main>
-        <HeaderXL headerXL="Home" />
         <HorizontalFlex childrenMarginRight={"2 rem"}>
           <PrimaryButton label="Primary Button" />
           <SecondaryButton label="Secondary Button" />
         </HorizontalFlex>
-
       </main>
       <footer>
         <Footer />
-
       </footer>
-
     </div>
   );
 }
