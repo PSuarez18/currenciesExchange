115b7943ed7569fc06c2757fb1b3a09b083ca860
jeermuce <ernesto_mc.dev@proton.me>
2024-01-08 21:00:42 -0600

    Add searchbar styles and icon

--- src/components/Searchbar/Searchbar.jsx
+++ src/components/Searchbar/Searchbar.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import HorizontalFlex from "../HorizontalFlex/HorizontalFlex";
-
+import "../../styles/searchbar.css";
+import icon from "../../assets/svg/magnifier.svg";
 function Searchbar({ onClick }) {
+  //{ children, spacing, height, width }
   return (
-    <HorizontalFlex>
-      <input type="text" placeholder="Search" />
-      <button type="submit" onClick={onClick}>
-        Search
+    <HorizontalFlex width={"25%"} height={"100%"}>
+      <input className="search-input" type="text" placeholder="Search" />
+      <button className="search-button" type="submit" onClick={onClick}>
+        <img className="search-icon" src={icon} alt="search icon" />
       </button>
     </HorizontalFlex>
   );
