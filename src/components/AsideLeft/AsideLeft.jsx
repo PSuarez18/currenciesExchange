6fb1405376a5c97b60b8b15f3ca0801c3dd29537
ptsuarez91@hotmail.com <ptsuarez91@hotmail.com>
2024-01-10 15:36:35 -0300

    mejore el responsive

--- src/components/AsideLeft/AsideLeft.jsx
+++ src/components/AsideLeft/AsideLeft.jsx
@@ -1,6 +1,33 @@
+import React, { useEffect } from 'react';
 import "../../styles/AsideRIght.css"
 
 const AsideLeft = () => {
+
+    const url = 'https://noticias-economia-espanol.p.rapidapi.com/api/v1/news/americaeconomia';
+    const options = {
+        method: 'GET',
+        headers: {
+            'X-RapidAPI-Key': '7ca99c9209mshc75808f70be92e1p133aa3jsn0c317be38284',
+            'X-RapidAPI-Host': 'noticias-economia-espanol.p.rapidapi.com'
+        }
+    };
+
+    async function callApi() {
+
+        try {
+            const response = await fetch(url, options);
+            const result = await response.text();
+            console.log(result);
+        } catch (error) {
+            console.error(error);
+        }
+    }
+    useEffect(() => {
+        callApi()
+
+    }, [])
+
+
     return (
         <div className="container-AsideRL">
             <article className="articles">
--- src/components/VideoInfo/VideoInfo.jsx
+++ src/components/VideoInfo/VideoInfo.jsx
@@ -8,9 +8,9 @@ const VideoInfo = () => {
                 className="frame-video"
                 src={urlYoutube}
                 title="YouTube video"
-                frameborder="0"
+                frameBorder="0"
                 allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-                allowfullscreen
+                allowFullScreen
             ></iframe>
         </div>
     )
