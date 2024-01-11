0985aa5764d111d16e460e790b0e692beb005692
jeermuce <ernesto_mc.dev@proton.me>
2024-01-10 21:39:28 -0600

    Correct AsideLeft and AsideRight css imports

--- src/components/AsideLeft/AsideLeft.jsx
+++ src/components/AsideLeft/AsideLeft.jsx
@@ -1,46 +1,43 @@
-import React, { useEffect } from 'react';
-import "../../styles/AsideRIght.css"
+import React, { useEffect } from "react";
+import "../../styles/AsideRight.css";
 
 const AsideLeft = () => {
+  const url =
+    "https://noticias-economia-espanol.p.rapidapi.com/api/v1/news/americaeconomia";
+  const options = {
+    method: "GET",
+    headers: {
+      "X-RapidAPI-Key": "7ca99c9209mshc75808f70be92e1p133aa3jsn0c317be38284",
+      "X-RapidAPI-Host": "noticias-economia-espanol.p.rapidapi.com",
+    },
+  };
 
-    const url = 'https://noticias-economia-espanol.p.rapidapi.com/api/v1/news/americaeconomia';
-    const options = {
-        method: 'GET',
-        headers: {
-            'X-RapidAPI-Key': '7ca99c9209mshc75808f70be92e1p133aa3jsn0c317be38284',
-            'X-RapidAPI-Host': 'noticias-economia-espanol.p.rapidapi.com'
-        }
-    };
-
-    async function callApi() {
-
-        try {
-            const response = await fetch(url, options);
-            const result = await response.text();
-            console.log(result);
-        } catch (error) {
-            console.error(error);
-        }
+  async function callApi() {
+    try {
+      const response = await fetch(url, options);
+      const result = await response.text();
+      console.log(result);
+    } catch (error) {
+      console.error(error);
     }
-    useEffect(() => {
-        callApi()
+  }
+  useEffect(() => {
+    callApi();
+  }, []);
 
-    }, [])
-
-
-    return (
-        <div className="container-AsideRL">
-            <article className="articles">
-                <h4 className="article-title">
-                    Noticias Diarias Finanzas
-                </h4>
-                <p className="article-text">
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Necessitatibus, blanditiis! Temporibus iure, voluptate necessitatibus facere illum deleniti alias quibusdam voluptatibus exercitationem assumenda id ducimus corporis. Nobis ipsa debitis quisquam animi.
-                </p>
-            </article>
-
-        </div>
-    )
-}
+  return (
+    <div className="container-AsideRL">
+      <article className="articles">
+        <h4 className="article-title">Noticias Diarias Finanzas</h4>
+        <p className="article-text">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit.
+          Necessitatibus, blanditiis! Temporibus iure, voluptate necessitatibus
+          facere illum deleniti alias quibusdam voluptatibus exercitationem
+          assumenda id ducimus corporis. Nobis ipsa debitis quisquam animi.
+        </p>
+      </article>
+    </div>
+  );
+};
 
 export default AsideLeft;
--- src/components/AsideRIght/AsideRight.jsx
+++ src/components/AsideRIght/AsideRight.jsx
@@ -1,32 +1,31 @@
 import VideoInfo from "../VideoInfo/VideoInfo";
-import "../../styles/AsideRIght.css"
+import "../../styles/AsideRight.css";
 
 const AsideRight = () => {
-    return (
-        <div className="container-AsideRL">
-            <aside>
-                <article className="articles">
-                    <h4 className="article-title">
-                        Poder de las Finanzas en la tus manos!
-                    </h4>
-                    <p className="article-text">
-                        Bienvenido a la App de Cambio de Divisas:
-                        ¿Te imaginas tener acceso instantáneo a las tasas de cambio de todas las divisas del mundo?
-                        ¡Nosotros lo hacemos posible!
-                    </p>
-                </article>
-                <VideoInfo />
-                <article className="articles">
-                    <p className="article-text">
-                        Desde el dólar hasta el yen, te ofrecemos un abanico completo de tasas de cambio.
-                        ¿Tienes una moneda específica en mente? ¡La encontrarás aquí!
-                    </p>
-                </article>
-
-            </aside>
-
-        </div>
-    )
-}
+  return (
+    <div className="container-AsideRL">
+      <aside>
+        <article className="articles">
+          <h4 className="article-title">
+            Poder de las Finanzas en la tus manos!
+          </h4>
+          <p className="article-text">
+            Bienvenido a la App de Cambio de Divisas: ¿Te imaginas tener acceso
+            instantáneo a las tasas de cambio de todas las divisas del mundo?
+            ¡Nosotros lo hacemos posible!
+          </p>
+        </article>
+        <VideoInfo />
+        <article className="articles">
+          <p className="article-text">
+            Desde el dólar hasta el yen, te ofrecemos un abanico completo de
+            tasas de cambio. ¿Tienes una moneda específica en mente? ¡La
+            encontrarás aquí!
+          </p>
+        </article>
+      </aside>
+    </div>
+  );
+};
 
 export default AsideRight;
